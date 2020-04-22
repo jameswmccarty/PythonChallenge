2776a894e5b53c4de4e997deb141925019415a30
jameswmccarty <jameswmccarty@gmail.com>
2020-04-22 13:23:49 -0400

    Add files via upload

---
+++ 15.py
@@ -0,0 +1,15 @@
+"""
+http://www.pythonchallenge.com/pc/return/uzi.html
+"""
+
+"""
+Clues: Year is 1XX6
+whom?
+Is a Leap Year (see 1796).
+<!-- he ain't the youngest, he is the second -->
+<!-- todo: buy flowers for tomorrow -->
+
+Significant birthdays: https://en.wikipedia.org/wiki/January_27#Births
+
+Mozart is on Jan 27, 1756.
+"""
