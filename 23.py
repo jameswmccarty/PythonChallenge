bb6d44a66fb03a86f2fe5fedd951fd76b27ab437
jameswmccarty <jameswmccarty@gmail.com>
2020-05-04 18:59:40 -0400

    Add files via upload

---
+++ 23.py
@@ -0,0 +1,51 @@
+"""
+http://www.pythonchallenge.com/pc/hex/bonus.html
+
+<!--
+TODO: do you owe someone an apology? now it is a good time to
+tell him that you are sorry. Please show good manners although
+it has nothing to do with this level.
+-->
+
+<!-- 	it can't find it. this is an undocumented module. -->
+
+<!--
+'va gur snpr bs jung?'  --> ROT13: in the face of what?
+-->
+
+title: what is this module?
+photo of cow near a road
+
+Soluiton: Had to search for Python Easter Eggs (not undocumented functions)
+The module 'this' prints the below poem.  The answer to the question is 'ambiguity'
+
+
+"""
+
+import this
+
+"""
+The Zen of Python, by Tim Peters
+
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!
+"""
+
+# ambiguity
